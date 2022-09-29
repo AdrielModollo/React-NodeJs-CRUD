dda0820805fc5b5305f091baf6e73228b4a7863c
AdrielModollo <adrielmodollo@outlook.com>
2022-09-28 22:11:44 -0300

    feat: app

--- react-crud/src/App.tsx
+++ react-crud/src/App.tsx
@@ -1,12 +1,26 @@
-const App = () => {
+import { Header } from './components/Header';
+import { Page, usePage } from './contexts/Page';
+
+const App: React.FC = () => {
+  const { page } = usePage();
+
+  const renderSwitch = () => {
+    switch (page) {
+      case Page.Articles:
+        return <div>Articles</div>;
+      case Page.Authors:
+        return <div>Authors</div>;
+      default:
+        return '';
+    }
+  };
+
   return (
     <div className="App">
-      <header>
-        <div className="title">Typescript CRUD</div>
-        <div className="subtitle">CRUD with React and TypeORM</div>
-      </header>
+      <Header />
+      {renderSwitch()}
     </div>
   );
-}
+};
 
 export default App;
